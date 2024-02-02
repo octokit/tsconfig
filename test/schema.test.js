65b226e17d37bc38f6c0a44283350404e4a3ab56
uzlopak <aras.abbasi@googlemail.com>
2024-02-02 20:22:29 +0100

    chore: add test

---
+++ test/schema.test.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const assert = require('node:assert')
+const fs = require('node:fs')
+const { test } = require('node:test')
+const Ajv = require("ajv-draft-04")
+
+const customKeywords = [
+  "allowTrailingCommas",
+  "markdownDescription",
+  "markdownEnumDescriptions"
+]
+
+test('validate tsconfig by schema', async () => {
+  const schema = await (await fetch('https://json.schemastore.org/tsconfig')).json()
+  const ajv = new Ajv({
+    strict: false
+  });
+
+  for (const keyword of customKeywords) {
+    ajv.addKeyword({
+      keyword
+    })
+  }
+
+  const validate = ajv.compile(schema)
+  const tsconfig = JSON.parse(await fs.promises.readFile('tsconfig.json', 'utf-8'))
+  assert(validate(tsconfig), 'tsconfig.json does not match the schema')
+})
